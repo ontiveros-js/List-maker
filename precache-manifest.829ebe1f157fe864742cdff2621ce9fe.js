5e4820e3ea347ddc5464a4dfb4a2822b513258ee
douglas ontiveros <douglasontiveros1408@gmail.com>
2022-01-21 03:22:04 -0300

    Updates

---
+++ precache-manifest.829ebe1f157fe864742cdff2621ce9fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1853618af92b2b8c3b2d3dbf668fa9",
+    "url": "/list-maker/index.html"
+  },
+  {
+    "revision": "6866d291bb0e918d7fad",
+    "url": "/list-maker/static/js/2.84201050.chunk.js"
+  },
+  {
+    "revision": "70380ff939c998672254",
+    "url": "/list-maker/static/js/3.b7dfee96.chunk.js"
+  },
+  {
+    "revision": "c9a69c0d23ff576a6cc9",
+    "url": "/list-maker/static/js/main.5b0991b5.chunk.js"
+  },
+  {
+    "revision": "a9420087b71cfe0e3861",
+    "url": "/list-maker/static/js/runtime~main.5af676a4.js"
+  }
+]);
